b1f8e158403c4a414296b43b0245cf21b17fe8f1
Rooarii <rooarii.manuel@gmail.com>
2021-01-30 21:49:06 +0100

    remove unecessary comments

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -3,9 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function UseEffect() {
   const [count, setCount] = useState(0);
 
-  // Similar to componentDidMount and componentDidUpdate:  
   useEffect(() => {    
-    // Update the document title using the browser API    
     document.title = `You clicked ${count} times`;  
   });
 
--- src/components/WaitAxiosRender.js
+++ src/components/WaitAxiosRender.js
@@ -1,16 +1,10 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
-// import Loading from './Loading'
 import { LoopCircleLoading} from 'react-loadingg';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
-  const [pokemon, setPokemon] = useState(
-    {
-      name:"",
-      sprites:{}
-    }
-    );
+  const [pokemon, setPokemon] = useState({name:"",sprites:{}});
 
   console.debug(
     `isLoading is ${isLoading} and pokemon is ${pokemon && pokemon.name}`
@@ -19,12 +13,8 @@ export default function App() {
   useEffect(() => {
     console.debug("After mount! Let's load data from API...");
     axios.get("https://pokeapi.co/api/v2/pokemon/9").then(response => {
-      console.debug("Hurray! We have Pokemon data, let's update our state");
-      console.debug("Calling setPokemon...");
-      console.log(response.data);
       const { name, sprites } = response.data;
       setPokemon({name, sprites});
-      console.debug("Calling setLoading...");
       setLoading(false);
     });
   }, []);
@@ -32,17 +22,12 @@ export default function App() {
   console.log(pokemon)
 
   if (isLoading) {
-    console.debug("renders: Loading...");
     return (
       <Fragment>
-        {/* <div className="App">Loading...</div>
-        <Loading /> */}
         <LoopCircleLoading />
       </Fragment>
     );
   }
-
-  console.debug("Finally!! A wild Pokémon appears :)");
 
   return (
     <div className="App">
