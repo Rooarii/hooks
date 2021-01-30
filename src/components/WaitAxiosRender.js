2e2d27b23ff9252dbee5c27b332fa9819909624b
Rooarii <rooarii.manuel@gmail.com>
2021-01-30 23:30:46 +0100

    get multiple axios

--- src/components/WaitAxiosRender.js
+++ src/components/WaitAxiosRender.js
@@ -1,38 +1,57 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
-// import Loading from './Loading'
+import Loading from './Loading'
 import { LoopCircleLoading} from 'react-loadingg';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
-  const [pokemon, setPokemon] = useState(
-    {
-      name:"",
-      sprites:{}
-    }
-    );
 
-  console.debug(
-    `isLoading is ${isLoading} and pokemon is ${pokemon && pokemon.name}`
-  );
+  const structure = {
+    name:"",
+    sprites:{}
+  }
+
+  const [pokemon, setPokemon] = useState( Object.create(structure));
+  const [pokemon1, setPokemon1] = useState(Object.create(structure));
+  
 
   useEffect(() => {
-    console.debug("After mount! Let's load data from API...");
-    axios.get("https://pokeapi.co/api/v2/pokemon/9").then(response => {
-      console.debug("Hurray! We have Pokemon data, let's update our state");
-      console.debug("Calling setPokemon...");
-      console.log(response.data);
-      const { name, sprites } = response.data;
-      setPokemon({name, sprites});
-      console.debug("Calling setLoading...");
-      setLoading(false);
+
+    const fetchPokemon1 = async () =>{
+      try {
+        await axios.get("https://pokeapi.co/api/v2/pokemon/1").then(response => {
+        const { name, sprites } = response.data;
+        setPokemon1({pokemon_1:name, sprites});
+        setLoading(false);
+        });
+      } catch (error) {
+        console.log(error.message)
+      }
+    };
+    
+    const fetchPokemon = async () =>{
+      try {
+        await axios.get("https://pokeapi.co/api/v2/pokemon/9").then(response => {
+        const { name, sprites } = response.data;
+        setPokemon({name, sprites});
+        setLoading(false);
     });
+      } catch (error) {
+        console.log(error.message)
+      }
+    };
+    
+   Promise.all([fetchPokemon(), fetchPokemon1()])
+    
+  //  fetchPokemon();
+  //  fetchPokemon1();
+
   }, []);
 
   console.log(pokemon)
+  console.log(pokemon1)
 
   if (isLoading) {
-    console.debug("renders: Loading...");
     return (
       <Fragment>
         {/* <div className="App">Loading...</div>
@@ -42,12 +61,13 @@ export default function App() {
     );
   }
 
-  console.debug("Finally!! A wild Pokémon appears :)");
-
   return (
     <div className="App">
-      <h1>{pokemon.name}</h1>
+      <h1>Pokemons</h1>
+      <h2>{pokemon.name}</h2>
       <img alt={pokemon.name} src={pokemon.sprites.front_default} />
+      <h2>{pokemon1.pokemon_1}</h2>
+      <img alt={pokemon1.pokemon_1} src={pokemon1.sprites.front_default} />
     </div>
   );
 }
